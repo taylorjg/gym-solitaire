230886f04eff3d45975fbd7d146edc5ef0a1300d
taylorjg <jonathan.taylor.programmer@gmail.com>
2020-11-27 20:55:57 +0000

    Refactoring (1)

--- gym_solitaire/envs/solitaire_env.py
+++ gym_solitaire/envs/solitaire_env.py
@@ -5,35 +5,12 @@ import numpy as np
 Location = namedtuple('Location', ['row', 'col'])
 Action = namedtuple('Action', ['from_location', 'via_location', 'to_location'])
 
-CENTRE = Location(3, 3)
-
 UP = 0
 DOWN = 1
 LEFT = 2
 RIGHT = 3
 
 DIRECTIONS = [UP, DOWN, LEFT, RIGHT]
-
-EMPTY_INFO = {}
-
-
-def create_board():
-    col_ranges = [
-        range(2, 5),
-        range(2, 5),
-        range(7),
-        range(7),
-        range(7),
-        range(2, 5),
-        range(2, 5)
-    ]
-    board = {}
-    for row, col_range in enumerate(col_ranges):
-        for col in col_range:
-            location = Location(row, col)
-            board[location] = True
-    board[CENTRE] = False
-    return board
 
 
 def follow_direction(location, direction):
@@ -49,21 +26,48 @@ def follow_direction(location, direction):
         return Location(row + 1, col)
 
 
-def make_all_actions(board):
-    actions = []
-    for from_location in board.keys():
+def iter_locations():
+    board_shape = [
+        '  XXX  ',
+        '  XXX  ',
+        'XXXXXXX',
+        'XXXXXXX',
+        'XXXXXXX',
+        '  XXX  ',
+        '  XXX  ',
+    ]
+    num_rows = len(board_shape)
+    num_cols = len(board_shape[0])
+    for row in range(num_rows):
+        for col in range(num_cols):
+            if board_shape[row][col] == 'X':
+                yield Location(row, col)
+
+
+def iter_actions():
+    for from_location in LOCATIONS:
         for direction in DIRECTIONS:
             via_location = follow_direction(from_location, direction)
             to_location = follow_direction(via_location, direction)
-            if via_location in board and to_location in board:
+            if via_location in LOCATIONS and to_location in LOCATIONS:
                 action = Action(from_location, via_location, to_location)
-                actions.append(action)
-    return actions
+                yield action
+
+
+def create_board():
+    board = {}
+    reset_board(board)
+    return board
+
+
+def reset_board(board):
+    for location in LOCATIONS:
+        board[location] = location != CENTRE
 
 
 def valid_action_indices(board):
     action_indices = []
-    for action_index, action in enumerate(ALL_ACTIONS):
+    for action_index, action in enumerate(ACTIONS):
         if is_valid_action(board, action):
             action_indices.append(action_index)
     return action_indices
@@ -71,25 +75,53 @@ def valid_action_indices(board):
 
 def is_valid_action(board, action):
     from_location, via_location, to_location = action
+    assert from_location in LOCATIONS
+    assert via_location in LOCATIONS
+    assert to_location in LOCATIONS
     return all([
-        from_location in board,
-        via_location in board,
-        to_location in board,
         board[from_location],
         board[via_location],
         not board[to_location]
     ])
 
 
-def observation_valid_actions(obs):
+def make_move(board, action):
+    from_location, via_location, to_location = action
+    assert from_location in LOCATIONS
+    assert via_location in LOCATIONS
+    assert to_location in LOCATIONS
+    assert board[from_location]
+    assert board[via_location]
+    assert not board[to_location]
+    board[from_location] = False
+    board[via_location] = False
+    board[to_location] = True
+
+
+def obs_to_board(obs):
     board = create_board()
     assert len(obs) == len(board)
-    for index, location in enumerate(board.keys()):
+    for index, location in enumerate(LOCATIONS):
         board[location] = bool(obs[index])
+    return board
+
+
+def board_to_obs(board):
+    values = []
+    for location in LOCATIONS:
+        values.append(board[location])
+    return np.array(values, dtype=np.float32)
+
+
+def observation_valid_actions(obs):
+    board = obs_to_board(obs)
     return valid_action_indices(board)
 
 
-ALL_ACTIONS = make_all_actions(create_board())
+CENTRE = Location(3, 3)
+LOCATIONS = list(iter_locations())
+ACTIONS = list(iter_actions())
+EMPTY_INFO = {}
 
 
 class SolitaireEnv(Env):
@@ -99,28 +131,26 @@ class SolitaireEnv(Env):
     def __init__(self):
         self._board = create_board()
         self.observation_space = spaces.Box(0, 1, (len(self._board),), np.float32)
-        self.action_space = spaces.Discrete(len(ALL_ACTIONS))
+        self.action_space = spaces.Discrete(len(ACTIONS))
 
     def seed(self, seed=None):
         return [seed]
 
     def reset(self):
-        for location in self._board.keys():
-            self._board[location] = True
-        self._board[CENTRE] = False
-        return self._make_observation()
+        reset_board(self._board)
+        return board_to_obs(self._board)
 
     def step(self, action_index):
-        obs = self._make_observation()
+        obs = board_to_obs(self._board)
         done = not valid_action_indices(self._board)
         if done:
             return obs, 0, True, EMPTY_INFO
-        assert 0 <= action_index < len(ALL_ACTIONS)
-        action = ALL_ACTIONS[action_index]
+        assert 0 <= action_index < len(ACTIONS)
+        action = ACTIONS[action_index]
         if not is_valid_action(self._board, action):
             return obs, -100, False, EMPTY_INFO
-        self._make_move(action)
-        obs = self._make_observation()
+        make_move(self._board, action)
+        obs = board_to_obs(self._board)
         done = not valid_action_indices(self._board)
         reward = self._calculate_final_reward() if done else 0
         return obs, reward, done, EMPTY_INFO
@@ -149,26 +179,3 @@ class SolitaireEnv(Env):
                 manhattan_distance_from_centre = row_diff + col_diff
                 reward -= manhattan_distance_from_centre
         return reward
-
-    def _make_observation(self):
-        values = list(self._board.values())
-        return np.array(values, dtype=np.float32)
-
-    def _make_move(self, action):
-        from_location, via_location, to_location = action
-        assert from_location in self._board
-        assert via_location in self._board
-        assert to_location in self._board
-        assert self._board[from_location]
-        assert self._board[via_location]
-        assert not self._board[to_location]
-        self._board[from_location] = False
-        self._board[via_location] = False
-        self._board[to_location] = True
-
-    # @staticmethod
-    # def valid_actions(obs):
-    #     board = create_board()
-    #     for index, location in enumerate(board.keys()):
-    #         board[location] = bool(obs[index])
-    #     return valid_action_indices(board)
